4c883d3c665dd6774113918ba8f4e42076f16fde
Hex-Zero <hex0@live.com>
2019-05-16 19:10:39 +0100

    Moved business to App, Add props.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import './App.css';
 import BusinessList from "../src/components/BusinessList/BusinessList";
 import SearchBar from "../src/components/SearchBar/SearchBar";
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [business,business,business,business,business,business];
 
 function App() {
   return (
       <div className="App">
           <h1>ravenous</h1>
           <SearchBar />
-          <BusinessList />
+          <BusinessList businesses={businesses}/>
       </div>
   );
 }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,13 +4,12 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component{
     render(){
         return (
-            <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
+            <div className="BusinessList"> 
+            
+            {this.props.businesses.map(business =>{
+                return <Business business={business}/>;
+            })}
+
             </div>
         )
     }
