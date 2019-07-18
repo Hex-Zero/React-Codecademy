3dc2b1a1def4796d711f66f60a3e765a1309a9e4
Hex-Zero <hex0@live.com>
2019-07-18 20:15:51 +0100

    Updates

---
+++ precache-manifest.a66a719050ca01ebc3ac1747f3a51cfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0284817d31cdfcb98fc1fa326e7332b",
+    "url": "/React-Codecademy/index.html"
+  },
+  {
+    "revision": "63170b7236df83b9f298",
+    "url": "/React-Codecademy/static/css/main.744f1a0b.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/React-Codecademy/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "63170b7236df83b9f298",
+    "url": "/React-Codecademy/static/js/main.44c54a3c.chunk.js"
+  },
+  {
+    "revision": "1e91bdcc352e7d54c574",
+    "url": "/React-Codecademy/static/js/runtime~main.82bc0d12.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/React-Codecademy/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/React-Codecademy/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
